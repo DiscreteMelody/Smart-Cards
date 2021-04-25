6001d2f5e8ebafd6aa1daa6e06a92a5af72fbdc4
Lucas Marsiske <47642996+LMarsiske@users.noreply.github.com>
2021-04-25 14:30:57 -0400

    Made some updates to the Help screen and recompiled another release exe

--- Smart Cards/Smart Cards/HelpPanel.Designer.cs
+++ Smart Cards/Smart Cards/HelpPanel.Designer.cs
@@ -24,84 +24,177 @@ namespace Smart_Cards {
 		/// the contents of this method with the code editor.
 		/// </summary>
 		private void InitializeComponent() {
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Century Gothic", 20.25F, System.Drawing.FontStyle.Bold);
-            this.label1.Location = new System.Drawing.Point(2, 7);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(74, 32);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Help";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(5, 49);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(137, 26);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "What is this?";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(30, 80);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(368, 20);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "It\'s Smart Cards, a digital flashcard learning system";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(5, 111);
-            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(174, 26);
-            this.label4.TabIndex = 3;
-            this.label4.Text = "How do I use it? ";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(30, 145);
-            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label5.MaximumSize = new System.Drawing.Size(338, 0);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(332, 60);
-            this.label5.TabIndex = 4;
-            this.label5.Text = "It\'s simple. You just create a new deck for whatever subject you want, then fill " +
-    "it full of as many cards as you need!";
-            // 
-            // HelpPanel
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.Name = "HelpPanel";
-            this.Size = new System.Drawing.Size(609, 464);
-            this.ResumeLayout(false);
-            this.PerformLayout();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HelpPanel));
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.label6 = new System.Windows.Forms.Label();
+			this.label7 = new System.Windows.Forms.Label();
+			this.label8 = new System.Windows.Forms.Label();
+			this.label9 = new System.Windows.Forms.Label();
+			this.label10 = new System.Windows.Forms.Label();
+			this.label11 = new System.Windows.Forms.Label();
+			this.panel1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Century Gothic", 20.25F, System.Drawing.FontStyle.Bold);
+			this.label1.Location = new System.Drawing.Point(0, 0);
+			this.label1.MaximumSize = new System.Drawing.Size(700, 0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(92, 40);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Help";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Location = new System.Drawing.Point(3, 50);
+			this.label2.MaximumSize = new System.Drawing.Size(700, 0);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(278, 31);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "What is Smart Cards?";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.Location = new System.Drawing.Point(25, 90);
+			this.label3.MaximumSize = new System.Drawing.Size(700, 0);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(699, 50);
+			this.label3.TabIndex = 2;
+			this.label3.Text = "Smart Cards is a digital flaschard learning system. It\'s designed to do everythin" +
+    "g you used to do with physical flashcards, only easier.";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label4.Location = new System.Drawing.Point(1, 169);
+			this.label4.MaximumSize = new System.Drawing.Size(700, 0);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(215, 31);
+			this.label4.TabIndex = 3;
+			this.label4.Text = "How do I use it? ";
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label5.Location = new System.Drawing.Point(25, 215);
+			this.label5.MaximumSize = new System.Drawing.Size(700, 0);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(691, 50);
+			this.label5.TabIndex = 4;
+			this.label5.Text = "It\'s simple! You just create a new deck for whatever subject you want, then fill " +
+    "it full of as many cards as you need.";
+			// 
+			// panel1
+			// 
+			this.panel1.AutoScroll = true;
+			this.panel1.Controls.Add(this.label11);
+			this.panel1.Controls.Add(this.label10);
+			this.panel1.Controls.Add(this.label9);
+			this.panel1.Controls.Add(this.label8);
+			this.panel1.Controls.Add(this.label7);
+			this.panel1.Controls.Add(this.label6);
+			this.panel1.Controls.Add(this.label1);
+			this.panel1.Controls.Add(this.label2);
+			this.panel1.Controls.Add(this.label3);
+			this.panel1.Controls.Add(this.label4);
+			this.panel1.Controls.Add(this.label5);
+			this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.panel1.Location = new System.Drawing.Point(0, 0);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(812, 1155);
+			this.panel1.TabIndex = 5;
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label6.Location = new System.Drawing.Point(3, 726);
+			this.label6.MaximumSize = new System.Drawing.Size(700, 0);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(662, 62);
+			this.label6.TabIndex = 5;
+			this.label6.Text = "How do I share decks, or use decks that others share with me?";
+			// 
+			// label7
+			// 
+			this.label7.AutoSize = true;
+			this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label7.Location = new System.Drawing.Point(25, 802);
+			this.label7.MaximumSize = new System.Drawing.Size(700, 0);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(695, 175);
+			this.label7.TabIndex = 6;
+			this.label7.Text = resources.GetString("label7.Text");
+			// 
+			// label8
+			// 
+			this.label8.AutoSize = true;
+			this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label8.Location = new System.Drawing.Point(3, 503);
+			this.label8.MaximumSize = new System.Drawing.Size(700, 0);
+			this.label8.Name = "label8";
+			this.label8.Size = new System.Drawing.Size(505, 31);
+			this.label8.TabIndex = 7;
+			this.label8.Text = "Can I search for decks or specific cards?";
+			// 
+			// label9
+			// 
+			this.label9.AutoSize = true;
+			this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label9.Location = new System.Drawing.Point(26, 550);
+			this.label9.MaximumSize = new System.Drawing.Size(700, 0);
+			this.label9.Name = "label9";
+			this.label9.Size = new System.Drawing.Size(698, 125);
+			this.label9.TabIndex = 8;
+			this.label9.Text = resources.GetString("label9.Text");
+			// 
+			// label10
+			// 
+			this.label10.AutoSize = true;
+			this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label10.Location = new System.Drawing.Point(3, 292);
+			this.label10.MaximumSize = new System.Drawing.Size(700, 0);
+			this.label10.Name = "label10";
+			this.label10.Size = new System.Drawing.Size(679, 62);
+			this.label10.TabIndex = 9;
+			this.label10.Text = "I see a save button when I am editing decks. Does that mean that my changes won\'t" +
+    " autosave?";
+			// 
+			// label11
+			// 
+			this.label11.AutoSize = true;
+			this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label11.Location = new System.Drawing.Point(26, 376);
+			this.label11.MaximumSize = new System.Drawing.Size(700, 0);
+			this.label11.Name = "label11";
+			this.label11.Size = new System.Drawing.Size(663, 100);
+			this.label11.TabIndex = 10;
+			this.label11.Text = resources.GetString("label11.Text");
+			// 
+			// HelpPanel
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.Controls.Add(this.panel1);
+			this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+			this.Name = "HelpPanel";
+			this.Size = new System.Drawing.Size(812, 1155);
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			this.ResumeLayout(false);
 
 		}
 
@@ -112,5 +205,12 @@ namespace Smart_Cards {
 		private System.Windows.Forms.Label label3;
 		private System.Windows.Forms.Label label4;
 		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Label label11;
+		private System.Windows.Forms.Label label10;
+		private System.Windows.Forms.Label label9;
+		private System.Windows.Forms.Label label8;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.Label label6;
 	}
 }
